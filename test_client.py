0e42b57aa16047070a57864e25df18be6dc2d6a5
Tamas Kiss <sauriaster@gmail.com>
2017-11-16 00:20:26 +0100

    client recive dictionary, bin data; server send dict, bin data

--- test_client.py
+++ test_client.py
@@ -1,5 +1,6 @@
 import socket
 import sys
+import json
 import tictactoe2
 from timeit import default_timer
 
@@ -28,31 +29,32 @@ def server_echo(s):
         print("the server responded")
 
 
-def socket_scaffold():
-    just_ping = False  #for testing the connection set to True
+def socket_scaffold():    
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     host = ip_address()
     port = 2100
     s.connect((host,port))
+    print("Server is started on", host, "port:", port)
+    return s    
 
-    if just_ping:
-        server_echo(s)
-        s.close()
-    else:
-        print("Server is started on", host, "port:", port)
-        return s    
-
-def send_names(s, player_name):
+def exchange_names(s, player_name):
     player_name = player_name.encode('utf-8')
     s.send(player_name)
-    date = s.recv(1024)
-    print(repr(date.decode('utf-8')))
+    second_player_name = s.recv(1024)
+    return second_player_name.decode('utf-8')
+    #print(repr(date.decode('utf-8')))
 
+def get_board(s):
     
-    
-    
+    board_dic_data = s.recv(4096)
+    print('board dic', board_dic_data)
+    board_decoded = board_dic_data.decode('utf-8')
+    print('board decoded', board_decoded)
+    board = json.loads(board_decoded)
+    print('board', board)
 
-
+    pass    #continue from here; todo: dict to list
+    #return board
 
 
 new_game = True
@@ -61,10 +63,11 @@ while new_game:
     start = default_timer()
     who_is_second_player = tictactoe2.choose_enemy()    
     player_one_mark, player_two_mark = tictactoe2.player_generator()
-    board = list("123456789")
-    reserve_list = []
+    
 
     if who_is_second_player == 0:
+        board = list("123456789")
+        reserve_list = []
         player_one_name = tictactoe2.player_names()
         player_two_name = tictactoe2.player_names()
         print("\nPlayer one is: ", player_one_name)        
@@ -79,6 +82,8 @@ while new_game:
         new_game = tictactoe2.start_new_game()
 
     elif who_is_second_player == 1:
+        board = list("123456789")
+        reserve_list = []
         player_one_name = tictactoe2.player_names()
         player_two_name = "Computer"        
         print("\nPlayer one is: ", player_one_name)        
@@ -95,7 +100,10 @@ while new_game:
     elif who_is_second_player == 2:
         s = socket_scaffold()
         player_one_name = tictactoe2.player_names()        
-        send_names(s, player_one_name) 
-        
+        player_two_name = exchange_names(s, player_one_name) 
+        print("\nPlayer one is: ", player_one_name)        
+        print("\nPlayer two is: ", player_two_name)
+        board = get_board(s)
+        print(board)        
         pass
             
--- test_server.py
+++ test_server.py
@@ -1,28 +1,51 @@
 import socket
 import time
+import json
+from random import randint
 
 def debug_print(message):
     if DEBUG_PRINT == True:
         print(message)
 
-def accept_connection():
-    connection, address = sockt.accept()
+def accept_connection(s):
+    connection, address = s.accept()
     print("Received a connection from", address[0], ":", port)
     return connection, address
+
+
+    
+    
+
     
 DEBUG_PRINT=True
 
-sockt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+debug_print("Server is running.")
+
+board = list("123456789")
+reserve_list = []
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 host = ''
 port = 2100
-sockt.bind((host,port))
-sockt.listen(5)
-first_connection, first_address = accept_connection()
-sec_connection, sec_address = accept_connection()
+s.bind((host,port))
+s.listen(5)
+first_connection, first_address = accept_connection(s)
+sec_connection, sec_address = accept_connection(s)
 name_one = first_connection.recv(1024)
 name_two = sec_connection.recv(1024)
 first_connection.sendall(name_two)
 sec_connection.sendall(name_one)
 
-sockt.close()
+json_obj = {'board': board}
+
+
+b_board = json.dumps(json_obj)
+dec_board = b_board.encode('utf-8')
+first_connection.sendall(dec_board)
+sec_connection.sendall(dec_board)
+
+
+
+debug_print("Server is up to shotdown")
+s.close()
